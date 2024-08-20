8d3fe6092436e89c2aa5cab589f0e96d21cf700d
Steve Le <osd_steve.le@nfcguest.com>
2024-08-20 09:39:01 +0700

    add j0 account

--- config/account.js
+++ config/account.js
@@ -23,7 +23,7 @@ export const getAccountList = async () => {
 
 export const isAccountDied = (address) => dieList.find((val) => val == address);
 export const isBankAccount = (adddress) =>
-  ["0", "a0", "b0", "c0", "d0", "e0", "f0", "g0", "h0", "i0", "bank11"].find((ba) => ba == adddress);
+  ["0", "a0", "b0", "c0", "d0", "e0", "f0", "g0", "h0", "i0", "j0", "bank11"].find((ba) => ba == adddress);
 export const MIN_SUI_PER_ACCOUNT = 0.015;
 export const AIRDROP_SUI_PER_ACCOUNT = 0.1;
 export const MIN_OCEAN_TO_SWAP = 1;
